94eea75eb1d67ccecaf22975fd3d70450b86b584
nagypeter095 <nagypeter095@gmail.com>
2023-07-24 11:23:55 +0200

    new logo

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -6,10 +6,10 @@ interface AvatarProps {
     src: string | null | undefined
 };
 
-const Avatar: React.FC<AvatarProps> = ({src}) => {
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
     return (
         <Image className="rounded-full" height="30" width="30" alt="Avatar" src={src || "/images/placeholder.jpg"} />
     );
 }
- 
+
 export default Avatar;
